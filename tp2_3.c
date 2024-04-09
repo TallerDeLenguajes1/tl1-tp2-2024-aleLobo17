4f148d0925e52c918194897c57968cc6b429c248
Lobo <lobo.rodrigoalejandro.2020@gmail.com>
2024-04-09 13:24:25 -0300

    punto3 (subo el archivo tp2_3.c modificado)

---
+++ tp2_3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 5
+#define M 7
+
+int main(){
+    int mt[N][M];
+    int *puntero;
+    puntero = mt;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            *puntero = 1+rand()%100;
+            //printf("%d ", *puntero);
+            puntero++;
+        }
+    }
+    
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            printf("%d ", mt[i][j]);
+        }
+        printf("\n");
+        
+    }
+    return 0;
+}
